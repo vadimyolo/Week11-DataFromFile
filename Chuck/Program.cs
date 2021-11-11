fca2f4a86e22dc2aba4f6696b63734edd5d97509
Opilane TTHK <opilane@tthk.ee>
2021-11-11 10:24:55 +0200

    datafrom file

---
+++ Chuck/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace Chuck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string directoryPath = @"C:\Users\opilane\Samples";
+            string fileName = "chuck.txt";
+            string fullPath = $@"{directoryPath}\{fileName}";
+
+            string[] dataFromFile = File.ReadAllLines(fullPath);
+            Random rnd = new Random();
+            Console.WriteLine(dataFromFile[rnd.Next(dataFromFile.Length)]);
+        }
+    }
+}
+
+
+
+
+
+
+            
+
+
+
+
+
+
+            
+
+            
+        
+
+        
---
+++ Horoscope/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+namespace DataFromFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your favorite horoscope");
+            string userHoroscope = Console.ReadLine();
+
+            string output = GetLineFromData(userHoroscope);
+
+            if (output.Length != 0)
+            {
+                Console.WriteLine(output);
+            }
+            else
+            {
+                Console.WriteLine("sorry, i don't know that sign.");
+            }
+
+        }
+        private static string[] ReadDataFromFile()
+        {
+            string directoryPath = @"C:\Users\opilane\Samples";
+            string fileName = "horoscope.txt";
+            string fullPath = $@"{directoryPath}\{fileName}";
+
+            string[] dataFromFile = File.ReadAllLines(fullPath);
+
+            return dataFromFile;
+        }
+
+        private static string GetLineFromData(string userInput)
+        {
+            string[] dataFromFile = ReadDataFromFile();
+            string result = "";
+
+            foreach (string line in dataFromFile)
+            {
+                if (line.Contains(userInput))
+                {
+                    result = line;
+                    break;
+
+                }
+            }
+            return result;
+        }
+    }
+    
+    
+}
+
+
